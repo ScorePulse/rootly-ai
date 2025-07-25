8800602ba2248ba74727e17111404f3c607b7ecc
Vishwas M S <vish.ms.005@gmail.com>
2025-07-25 18:54:30 +0530

    feat: firbase setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["logo.svg"],
+      manifest: {
+        name: "Vite PWA Project",
+        short_name: "Vite PWA Project",
+        description: "I am a simple Vite PWA",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: "icons/logo.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 5174,
   },
-})
+});
